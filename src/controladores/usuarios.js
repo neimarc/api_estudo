f6887fb9bc3aee9a8c13d9bb90d658493efe0a92
neimarc <neimarpersonal@gmail.com>
2024-10-22 00:04:07 -0300

    Mudança de lógica

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,37 +1,73 @@
-const { knex } = require('');
+// const { knex } = require('');
 const { saudarSchema } = require('../validacoes.js/usuarios.squema');
 
 
 const saudar = async (req, res) => {
 
-    let { nome, sexo, vezes } = req.body;
+    let { nome_usuario, sexo_usuario, pergunto_quantas_vezes, apresentar_IA } = req.body;
 
 
     try {
         await saudarSchema.validate(req.body);
 
-        nome = nome.trim();
-        sexo = sexo.trim();
+        nome_usuario = nome_usuario.trim();
+        sexo_usuario = sexo_usuario.trim();
+        apresentar_IA = apresentar_IA.trim();
+
 
         const resposta = [];
-        for (let i = 0; i < vezes; i++) {
-            if (sexo == "masculino") {
-                const saudacao = `Olá, ${nome}! 
-                Está disposto a aprender sobre tecnologia hoje, meu rapaz?`;
-                resposta.push(saudacao)
-            } else if (sexo == "feminino") {
-                const saudacao = `Olá, ${nome}! 
-                Está disposta a aprender sobre tecnologia hoje, minha senhora?`;
-                resposta.push(saudacao)
+
+        if (apresentar_IA) {
+            if (apresentar_IA == "Sim" || apresentar_IA == "sim") {
+                const ola = `Olá, ${nome_usuario}!`;
+                const apresentacao = `Eu sou a versão IA da Ellen Santos, estudante do curso técnico em Astrologia e estagiária da SmartLevel. Tenho 40 anos de idade, quatro filhos (um cachorro, um gato, um rato e uma cobra) e o meu objetivo é ficar rica e ir morar em Dubai.`;
+
+                resposta.push(ola);
+                resposta.push(apresentacao)
             } else {
-                res.status(404).send(`Desculpe, ${nome}, mas eu não entendi o que você é`);
+                const ola = `Olá, ${nome_usuario}!`;
+                const apresentacao = `Que pena que você não quis a minha apresentação!`;
+
+                resposta.push(ola);
+                resposta.push(apresentacao)
             }
         }
 
-        res.status(204).send(resposta)
+        for (let i = 0; i < pergunto_quantas_vezes; i++) {
+            if (sexo_usuario == "masculino" || sexo_usuario == "Masculino") {
+                if (pergunto_quantas_vezes == 1) {
+                    const saudacao = `Então, ${nome_usuario}: Está disposto a aprender sobre tecnologia hoje, meu rapaz ? `;
+                    resposta.push(saudacao)
+                } else {
+                    const saudacao = `Então, ${nome_usuario}: Está disposto a aprender sobre tecnologia hoje, meu rapaz ? `;
+                    const repetir = "Vou repetir:";
+
+                    resposta.push(saudacao)
+                    if (i < pergunto_quantas_vezes - 1)
+                        resposta.push(repetir);
+                }
+
+            } else if (sexo_usuario == "feminino" || sexo_usuario == "Feminino") {
+                if (pergunto_quantas_vezes == 1) {
+                    const saudacao = `Então, ${nome_usuario}: Está disposto a aprender sobre tecnologia hoje, minha senhora ? `;
+                    resposta.push(saudacao)
+                } else {
+                    const saudacao = `Então, ${nome_usuario}: Está disposto a aprender sobre tecnologia hoje, minha senhora ? `;
+                    const repetir = "Vou repetir:";
+
+                    resposta.push(saudacao)
+                    if (i < pergunto_quantas_vezes - 1)
+                        resposta.push(repetir);
+                }
+            } else {
+                res.status(404).send(`Desculpe, ${nome_usuario}, mas eu não entendi o que você é`);
+            }
+        }
+
+        res.status(200).json(resposta)
 
     } catch (erro) {
-        res.status(500).json({ mensagem: `Erro no servidor. Tente novamente mais tarde.` })
+        res.status(500).json({ mensagem: `Erro no servidor.Tente novamente mais tarde.` })
     }
 
 }
