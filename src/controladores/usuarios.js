03c2b35624035596a4d61b98868d9b88ef80b021
neimarc <neimarpersonal@gmail.com>
2024-10-22 22:28:12 -0300

    Outras adições

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -20,26 +20,30 @@ const saudar = async (req, res) => {
         if (apresentar_IA) {
             if (apresentar_IA == "Sim" || apresentar_IA == "sim") {
                 const ola = `Olá, ${nome_usuario}!`;
-                const apresentacao = `Eu sou a versão IA da Ellen Santos, estudante do curso técnico em Astrologia e estagiária da SmartLevel. Tenho 40 anos de idade, cinco animais (um cachorro, um gato, um rato, uma cobra e o namorado). Sou gótica, de Áries e gosto de rock. Estou estudando horrores, neste ano (pegando 32 matérias), porque o meu objetivo é ficar rica e ir morar em Dubai. Mas por hora, no estágio, tenho que aturar um chato, com nome de jogador famoso, de futebol, que, a cada manhã, me pede para instalar alguma coisa em minha máquina. Ai, socorro!!!`;
-
+                const apresentacao = `Eu sou a versão IA da Ellen Santos, estudante do curso técnico em Astrologia e estagiária da SmartLevel. Atualmete, moro em Barreiras, tenho 40 anos de idade, cinco animais (um cachorro, um gato, um rato, uma cobra e um namorado), sou gótica, de Áries e, nas horas vagas, gosto de ouvir rock e assistir filmes e séries de terror e ficção científica. Estou estudando horrores neste ano (pegando 34 matérias), mais que o Mark Zuckerberg nos tempos da Havard, porque o meu objetivo é ficar rica e ir morar em Dubai. Mas por hora, no estágio, estou tendo que aturar um chato, com nome de jogador famoso, de futebol, que, a cada manhã, me pede para instalar alguma coisa em minha máquina, e à noite, me arruma mais dores de cabeça no meu horário de estudo. Ai, socorro, meu Deus do céu!!!`;
+                const Aperguntar = `Sem mais sobre mim, eu lhe pergunto:`;
                 resposta.push(ola);
-                resposta.push(apresentacao)
+                resposta.push(apresentacao);
+                resposta.push(Aperguntar)
+
             } else {
                 const ola = `Olá, ${nome_usuario}!`;
-                const apresentacao = `Que pena que você não quis que eu me apresentasse, meu bem! Já que não sou rancorosa, vou lhe perguntar assim mesmo:`;
-
+                const apresentacao = `Que pena que você não quis que eu me apresentasse, meu bem!`;
+                const Aperguntar = `Já que não sou rancorosa, vou lhe perguntar assim mesmo:`;
                 resposta.push(ola);
-                resposta.push(apresentacao)
+                resposta.push(apresentacao);
+                resposta.push(Aperguntar)
+
             }
         }
 
         for (let i = 0; i < pergunto_quantas_vezes; i++) {
             if (sexo_usuario == "masculino" || sexo_usuario == "Masculino") {
                 if (pergunto_quantas_vezes == 1) {
-                    const saudacao = `Então, ${nome_usuario}: Está disposto a aprender sobre tecnologia hoje, meu querido? `;
+                    const saudacao = `${nome_usuario}: Está disposto a aprender sobre tecnologia hoje, meu querido? `;
                     resposta.push(saudacao)
                 } else {
-                    const saudacao = `Então, ${nome_usuario}: Está disposto a aprender sobre tecnologia hoje, meu querido? `;
+                    const saudacao = `${nome_usuario}: Está disposto a aprender sobre tecnologia hoje, meu querido? `;
                     const repetir = "Vou repetir:";
 
                     resposta.push(saudacao)
@@ -49,10 +53,10 @@ const saudar = async (req, res) => {
 
             } else if (sexo_usuario == "feminino" || sexo_usuario == "Feminino") {
                 if (pergunto_quantas_vezes == 1) {
-                    const saudacao = `Então, ${nome_usuario}: Está disposta a aprender sobre tecnologia hoje, minha querida? `;
+                    const saudacao = `${nome_usuario}: Está disposta a aprender sobre tecnologia hoje, minha querida? `;
                     resposta.push(saudacao)
                 } else {
-                    const saudacao = `Então, ${nome_usuario}: Está disposta a aprender sobre tecnologia hoje, minha querida? `;
+                    const saudacao = `${nome_usuario}: Está disposta a aprender sobre tecnologia hoje, minha querida? `;
                     const repetir = "Vou repetir:";
 
                     resposta.push(saudacao)
